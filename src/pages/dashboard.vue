2cbfc9fa59caa9e97234fdeffe4d845880874459
BOGZS <mandericoprinceedgar@gmail.com>
2024-02-01 17:18:54 +0800

    Finished Login Form

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <marquee behavior="" direction="" scrollamount="20">
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        <h1>THIS IS THE DASHBOARD</h1>
+        <br>
+        </marquee>
+    </div>
+</template>
